15aa5dfdcd5c9638d5ab06b65af090ab0e8e1bd9
a.chebotarev <a.chebotarev@enbek.kz>
2022-11-25 11:17:18 +0600

    123

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use gpacks\libs\ui;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-pack', function () {
+    
+    $pack = ui::t('My first lib');
+    
+    dd($pack);
+
 });
